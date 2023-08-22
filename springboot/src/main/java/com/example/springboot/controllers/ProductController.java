69ef92a283cc1667046c372b2afb3fab41a748d1
josehigor27 <josehigor27@gmail.com>
2023-08-22 19:58:38 -0300

    feat: Create ProductController Function Delete

--- springboot/src/main/java/com/example/springboot/controllers/ProductController.java
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -54,5 +54,15 @@ public class ProductController {
 
     }
 
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID uuid){
+        Optional<ProductModel> product = productRepository.findById(uuid);
+        if (product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
+
 
 }
